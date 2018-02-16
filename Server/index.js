3047a07789954f49d96c10a6b9aa699e9f6053d8
chris75898 <chris75898@gmail.com>
2018-02-16 14:26:49 -0600

    Fixed bug with sending admin updates

--- Server/index.js
+++ Server/index.js
@@ -85,7 +85,7 @@ function initializeServer()
 		{
 			if (config[eachItem].isHidden)
 				continue;
-			html += "<span>" + (config[eachItem].label || eachItem) + "</span><input type='text' name='" + encodeURIComponent(config[eachItem]) + "' value='" + encodeURIComponent(config[eachItem].value || config[eachItem].defaultValue || "") + "' /><br />";
+			html += "<span>" + (config[eachItem].label || eachItem) + "</span><input type='text' name='" + config[eachItem] + "' value='" + encodeURIComponent(config[eachItem].value || config[eachItem].defaultValue || "") + "' /><br />";
 		}
 		html += "<button type=submit>Save</button></form>"
 		html += "<a href='/admin/Client?type=Screen'>Download Screen Script</a><br />"
@@ -96,7 +96,7 @@ function initializeServer()
 		res.send(html);
 	});
 	apiRoutes.post("/admin", function(req, res){
-		var config = fs.readFileSync(path.join(__dirname, "Config/config.json"));
+		var config = fs.readFileSync(path.join(__dirname, "Config/config.json"), "utf8");
 		try {config = JSON.parse(config);} catch(err) {return res.sendStatus(403);}
 
 		if (!req.body)
