83293c1bf29fc60e0b725127d0400c82e78f0822
chris75898 <chris75898@gmail.com>
2018-02-16 12:28:45 -0600

    Index has been updated to have a admin side, stored in the config.json file

--- Server/index.js
+++ Server/index.js
@@ -8,15 +8,39 @@ var pem  = require("pem");
 var https = require('https');
 var msg = require("./messaging.js");
 var rooms = require("./rooms.js");
+var info = require("../package.json");
+var targz = require('tar.gz');
+var crypto = require('crypto');
+var randomstring = require("randomstring");
+var session = require('express-session')
+var bodyParser = require('body-parser');
+
 
 //
 //Startup
 //
 var app = express();
+var apiRoutes = express.Router();
+
+//Verify Config File
+var configFilePath = path.join(__dirname, "Config/config.json");
+if (fs.existsSync(configFilePath))
+{
+	var config = fs.readFileSync(configFilePath, "utf8");
+	config = JSON.parse(config);
+}
+else config = {};
+var requiredConfigParams = ["version", "OrganizationName", "QuickAddress", "adminUserName", "adminUserSalt", "adminUserPasswordHash", "ChromeExtensionUrl", "androidAppUrl", "iosAppUrl", "AllHostingScreenFromServer"];
+for (var i=0; i<requiredConfigParams.length; i++)
+{
+	if (!(requiredConfigParams[i] in config))
+		config[requiredConfigParams[i]] = null;
+}
+fs.writeFileSync(path.join(__dirname, "Config/config.json"), JSON.stringify(config));
 
 //Make sure certificates exists
-var tlsKeyPath = __dirname + "/Config/server.key";
-var tlsCertPath = __dirname + "/Config/server.crt";
+var tlsKeyPath = path.join(__dirname, "Config/server.key");
+var tlsCertPath =  path.join(__dirname, "Config/server.crt");
 var tlsOptions = {};
 if (!fs.existsSync(tlsKeyPath) || !fs.existsSync(tlsCertPath))
 {
@@ -25,16 +49,95 @@ if (!fs.existsSync(tlsKeyPath) || !fs.existsSync(tlsCertPath))
 		msg.logDebug("SSL Certificate could not be found, generating self-signed certificates");
 		tlsOptions.key = keys.serviceKey;
 		tlsOptions.cert = keys.certificate;
-		startServer();
+		initializeServer();
 	});
 }
 else
 {
 	tlsOptions.key = fs.readFileSync(tlsKeyPath);
 	tlsOptions.cert = fs.readFileSync(tlsCertPath);
-	startServer();
+	initializeServer();
 }
 
+function initializeServer()
+{
+	function verifyAuthentication(req, res, next)
+	{
+		if (req.url.startsWith("/admin") && (!req.session || !req.session.authenticated))
+			return res.redirect("/login");
+		next();
+	}
+
+	apiRoutes.get("/admin", function(req, res){
+		var config = fs.readFileSync(path.join(__dirname, "Config/config.json"), "utf8");
+		config = JSON.parse(config);
+		var html = "<html><body><form method='POST'>";
+		
+		for(var eachItem in config)
+			html += "<span>" + eachItem + "</span><input type='text' name='" + eachItem + "' value='" + (config[eachItem] || "") + "' /><br />";
+		html += "<button type=submit>Save</button></form>"
+		html += "<a href='/admin/Client/Screen'>Download Screen Script</a>"
+		html += "</body></html>"
+		res.send(html);
+	});
+	apiRoutes.post("/admin", function(req, res){
+		var config = fs.readFileSync(path.join(__dirname, "Config/config.json"));
+		try {config = JSON.parse(config);} catch(err) {return res.sendStatus(403);}
+
+		if (!req.body)
+			return res.sendStatus(200);
+
+		for (var eachParam in req.body)
+			config[eachParam] = req.body[eachParam];
+
+		fs.writeFileSync(path.join(__dirname, "Config/config.json"), JSON.stringify(config));
+		res.sendStatus(200);
+	});
+	apiRoutes.get("/login", function(req,res){
+		var html = "<html><body><form method='POST'>";
+		html += "<input type='text' name='username' /><input type='password' name='password' /><button type='submit'>Login</button>";
+		html += "</form></body></html>"
+		res.send(html);
+	});
+	apiRoutes.post("/login", function(req, res){
+		var config = fs.readFileSync(path.join(__dirname, "Config/config.json"));
+		config = JSON.parse(config);
+
+		if (!req.body.username)
+			return res.send("Unauthorized");
+		if (config.adminUserName && (req.body.username !== config.adminUserName))
+			return res.send("Unauthorized");
+		if ((!config.adminUserName) && (req.body.username !== "admin"))
+			return res.send("Unauthorized");
+		if (config.adminUserPasswordHash && ((!req.body.password) || crypto.createHash("sha512").update(adminUserSalt + req.body.password) !== config.adminUserPasswordHash))
+			return res.send("Unauthorized");
+
+		req.session.authenticated = true;
+		return res.redirect('/admin'); 
+	});
+	app.get("/admin/Client/Screen", function(req, res)
+	{
+		var filePath = path.join(__dirname, "../Clients/Packages/Screen_" + info.version);
+		if (fs.existsSync(filePath))
+			return res.sendFile(filePath);
+
+		var compress = new targz().compress(path.join(__dirname, "../Clients/Screen"), filePath, function(err)
+		{
+			res.sendFile(filePath);
+		});
+
+	});
+
+	app.get("/Version", function(req,res){
+		res.send(info.version);
+	});
+	app.use(express.static(path.join(__dirname, 'Public')));
+	app.use(bodyParser.urlencoded({ extended: false }));
+	app.use(session({ secret: randomstring.generate(), resave: true, saveUninitialized: false}));
+	app.use(verifyAuthentication);
+	app.use(apiRoutes);
+	startServer();
+}
 function startServer()
 {
 	var server = https.createServer(tlsOptions, app).listen(443, function() 
@@ -105,5 +208,3 @@ function onNewConnection(ws)
     msg.logDebug("New Connection: " + sessionCounter.toString());
     return sessionCounter.toString();
 }
-
-app.use(express.static(path.join(__dirname, 'Static')));
